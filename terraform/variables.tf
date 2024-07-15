98600701c7dead3cd4f5fdb50359cd93450a4c3d
Mariana Alcantara <marigigialcantara40@gmail.com>
2024-07-15 09:55:08 -0300

    update variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,3 @@
-variable "ARM_SUBSCRIPTION_ID" {
-  description = "The subscription ID for Azure"
-  type        = string
-}
-
-variable "ARM_CLIENT_ID" {
-	description = "The client ID for Azure"
-	type        = string
-}
-
-variable "ARM_CLIENT_SECRET" {
-  description = "The client secret for Azure"
-  type        = string
-  sensitive   = true
-}
-
-variable "ARM_TENANT_ID" {
-  description = "Tenant ID for Azure"
-  type        = string
-}
-
 variable "location" {
   description = "The Azure location for resources"
   type        = string
